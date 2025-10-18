452807ba51ec4f816f7e89c3a6cef5bdbb3cd450
rasita101 <rasita.s66@rsu.ac.th>
2025-10-18 12:20:28 +0700

    Chnage name

--- app/components/attractions.js
+++ app/components/attractions.js
@@ -49,7 +49,7 @@ export default function Attractions() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">
-        Popular Tourist Attractions
+        Rasita Srivilaived 6603105
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
